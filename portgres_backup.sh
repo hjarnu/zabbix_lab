d836e2f7b0c946ea4142ab88751e528be573a7d4
Varjostaja <60552856+hjarnu@users.noreply.github.com>
2023-12-15 14:29:12 +0600

    Update portgres_backup.sh

--- portgres_backup.sh
+++ portgres_backup.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-container=zabbix_db
+container=zabbix-db
 dump=zabbix.tar
 local_dir=/backups/postgres/
-archive=/backups/postgres/zabbix_$(date +%d-%h-%Y_%H%M%S).tar.gz
+archive=${local_dir}zabbix_$(date +"%d-%h-%Y_%H%M%S").tar.gz
+log_file=/var/log/postgres_backup.log
 
-docker exec $container bash -c "pg_dump -Ft -U zabbix zabbix > /${dump}" \
-&& docker cp ${container}:/${dump} ${local_dir}/${dump} \
-&& gzip ${local_dir}/${dump} \
-&& mv ${local_dir}/${dump}.gz $archive
+# Function for logging with timestamp
+log() {
+  echo "$(date +"%Y-%m-%d %H:%M:%S") $1" >> $log_file
+}
+
+# Database dump
+if docker exec $container bash -c "pg_dump -Ft -U zabbix zabbix > /${dump}" >> $log_file 2>&1; then
+  log "Database dump completed successfully"
+  docker cp ${container}:/${dump} ${local_dir}/${dump}
+  log "Database dump file copied successfully to the local directory"
+else
+  log "Error: Database dump failed"
+  exit 1
+fi
+
+# Move and compress the dump
+gzip ${local_dir}${dump} && mv ${local_dir}${dump}.gz ${archive}
+log "Backup saved to: ${archive}"
+
+# Delete the dump file inside the container
+docker exec $container bash -c "rm -f /${dump}" >> $log_file 2>&1
+log "Dump file deleted inside the container"
