bc0c1d71b4f674cdf402e492abce9bcae5e8580c
Varjostaja <60552856+hjarnu@users.noreply.github.com>
2023-12-15 17:07:20 +0600

    Update portgres_backup.sh

--- portgres_backup.sh
+++ portgres_backup.sh
@@ -3,7 +3,6 @@
 container=zabbix-db
 dump=zabbix.tar
 local_dir=/backups/postgres/
-archive=${local_dir}zabbix_$(date +"%d-%h-%Y_%H%M%S").tar.gz
 log_file=/var/log/postgres_backup.log
 
 # Function for logging with timestamp
@@ -14,6 +13,7 @@ log() {
 # Database dump
 if docker exec $container bash -c "pg_dump -Ft -U zabbix zabbix > /${dump}" >> $log_file 2>&1; then
   log "Database dump completed successfully"
+  archive="${local_dir}zabbix_$(date +"%d-%h-%Y_%H%M%S").tar.gz"
   docker cp ${container}:/${dump} ${local_dir}/${dump}
   log "Database dump file copied successfully to the local directory"
 else
@@ -28,3 +28,11 @@ log "Backup saved to: ${archive}"
 # Delete the dump file inside the container
 docker exec $container bash -c "rm -f /${dump}" >> $log_file 2>&1
 log "Dump file deleted inside the container"
+
+# Retain only the latest three archive files
+older_archives=$(ls -t ${local_dir} | grep -E '^zabbix_[0-9]{2}-[a-zA-Z]{3}-[0-9]{4}_[0-9]{6}\.tar\.gz' | tail -n +4)
+for older_archive in $older_archives; do
+  log "Removing older archive: ${local_dir}${older_archive}"
+  rm "${local_dir}${older_archive}"
+  log "Older archives removed, keeping only the latest three"
+done
